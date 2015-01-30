ba62ae26ff94ca4972345ce90ae148b231076795
Ripounet <rip0320@yahoo.com>
2015-01-30 22:46:35 +0100

    Business App java : fix non-compiling stubs.

--- business-app/java/src/businessapp/BusinessAppServlet.java
+++ business-app/java/src/businessapp/BusinessAppServlet.java
@@ -15,36 +15,9 @@ import java.util.Date;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 
 public class BusinessAppServlet extends HttpServlet {
-	
-    public class BusinessAppServlet extends HttpServlet {
-		
-	    @Override
-	    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-	                throws IOException {
-	    	
-	    }
-	    
-	    @Override
-	    public void doPost(HttpServletRequest req, HttpServletResponse resp)
-	                throws IOException {
-	    	/*
-	        Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
-	        String content = req.getParameter("content");
-	        Entity greeting = new Entity("Greeting", guestbookKey);
-	        greeting.setProperty("user", user);
-	        greeting.setProperty("date", date);
-	        greeting.setProperty("content", content);
-	
-	        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-	        datastore.put(greeting);
-	
-	        resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
-	    	 */
-	    	req.getRequestDispatcher("/list.jsp").forward(req,res);
-	    }
-	}
 
 	@Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -54,7 +27,7 @@ public class BusinessAppServlet extends HttpServlet {
     
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
-                throws IOException {
+                throws IOException, ServletException {
     	/*
         Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
         String content = req.getParameter("content");
