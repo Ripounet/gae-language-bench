d8f97706f1e752b346a3393cb0db9cc69154e669
Ripounet <rip0320@yahoo.com>
2015-01-31 14:23:02 +0100

    Business App java working.

--- business-app/java/src/businessapp/GopherFacade.java
+++ business-app/java/src/businessapp/GopherFacade.java
@@ -5,7 +5,7 @@ public class GopherFacade {
 	String name;
 	public String getId(){ return id; }
 	public String getName(){ return name; }
-	public void setId(){}
-	public void setName(){}
+	public void setId(String i){id=i;}
+	public void setName(String n){name=n;}
 	public GopherFacade(String i, String n){ id=i; name=n; }
 }
