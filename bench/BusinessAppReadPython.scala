f71d5f4cadaf77df82e512563110588880e3a79a
Ripounet <rip0320@yahoo.com>
2015-01-31 02:41:10 +0100

    Bench BA py RO, RW.

--- bench/BusinessAppReadPython.scala
+++ bench/BusinessAppReadPython.scala
@@ -7,11 +7,10 @@ import io.gatling.jdbc.Predef._
 
 import util.Random
 
-class BusinessAppWritePython extends Simulation {
+class BusinessAppReadPython extends Simulation {
 
 	val httpProtocol = http
-//		.baseURL("http://python.bench-business-app.appspot.com")
-		.baseURL("http://localhost:8080")
+		.baseURL("http://python.bench-business-app.appspot.com")
 		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 		.acceptEncodingHeader("gzip, deflate")
 		.acceptLanguageHeader("en-US,en;q=0.5")
@@ -19,14 +18,16 @@ class BusinessAppWritePython extends Simulation {
 		.contentTypeHeader("application/x-www-form-urlencoded")
 		.userAgentHeader("Valentin load test with Gatling")
 
-	val scn = scenario("BusinessAppWrite")
-		.exec(http("request_3")
-			.post("/save")
-			.formParam("id", _ => 100 + Random.nextInt(900) )
-			.formParam("name", _ => "RandomName" + (100 + Random.nextInt(900))))
+	val scn = scenario("BusinessAppRead")
+		.exec(http("request_0")
+			.get("/"))
+		.exec(http("request_1")
+			.get("/detail")
+			.queryParam("id", _ => 100 + Random.nextInt(900)))
 
 	setUp(scn.inject(
-//		rampUsersPerSec(0.2) to(4) during(20 seconds)
-		rampUsersPerSec(0.2) to(1) during(6 minutes)
+//		rampUsersPerSec(0.2) to(1) during(10 seconds)
+		rampUsersPerSec(0.2) to(10) during(6 minutes)
 		)).protocols(httpProtocol)
+			
 }
